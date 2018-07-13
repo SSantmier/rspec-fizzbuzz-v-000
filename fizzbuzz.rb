90ddc168c111879630eac91a6c77afaa3ff04d5b
Sara S <ssantmier@gmail.com>
2018-07-13 17:47:03 +0000

    automatically backed up by learn

--- fizzbuzz.rb
+++ fizzbuzz.rb
@@ -6,12 +6,7 @@ require_relative './spec_helper.rb'
 def fizzbuzz?(int)
   if int % 3 == 0
     puts "Fizz"
-  elsif int % 5 == 0
-    puts "Buzz"
-  elsif  int % 5 == 0 && int 3 == 0
-     puts "FizzBuzz"
-  else
-      puts "nil"
+  end
 end
   #'returns "Fizz" when the number is divisible by 3' 
   #returns nil when the number is not divisible by 3 or 5'
